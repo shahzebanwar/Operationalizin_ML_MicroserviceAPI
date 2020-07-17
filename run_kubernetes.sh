6b504d4e3442026a7feeff59535a4ae9369be5cf
shahzebanwar <shahzebanwer@gmail.com>
2020-07-18 00:14:07 +0500

    Completed run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath = shahzeb01/ml-ms-api
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-ms-api --image=shahzeb01/ml-ms-api:development --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployment ml-ms-api --type=LoadBalancer --port=8000 --target-port=80
+kubectl port-forward deployment/ml-ms-api 8000:80
 
