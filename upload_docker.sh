e6654b252423f436bf2cda9dd00520818db7ce6e
shahzebanwar <shahzebanwer@gmail.com>
2020-07-17 22:49:29 +0500

    Edited upload docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath = shahzeb01/mlapi
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login && docker image tag mlapi $dockerpath
+
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
